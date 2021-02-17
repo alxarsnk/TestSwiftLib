72b7be439185da34eb600a0f4a108a477ef3bb30
Alexandr Arsenyuk <aleksandr@startte.com>
2021-02-17 12:30:27 +0300

    Initial Commit

---
+++ TestSwiftLib/TestSwiftLib.h
@@ -0,0 +1,18 @@
+//
+//  TestSwiftLib.h
+//  TestSwiftLib
+//
+//  Created by Alexandr Arsenyuk on 17.02.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestSwiftLib.
+FOUNDATION_EXPORT double TestSwiftLibVersionNumber;
+
+//! Project version string for TestSwiftLib.
+FOUNDATION_EXPORT const unsigned char TestSwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSwiftLib/PublicHeader.h>
+
+
